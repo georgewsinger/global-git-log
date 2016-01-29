af1bb1bd60f51a4bfd77bfe14278de585406c0c5
George Singer <george.w.singer@gmail.com>
2016-01-29 10:46:48 -0500

    Require debug fix attempt.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var argv = require('minimist')(process.argv.slice(2));
+var min = require('minimist');
+var argv = min(process.argv.slice(2));
 var exec = require('child_process').exec;
 var one = argv["_"][0] ? argv["_"][0] : "1.day";
 var two = argv["_"][1] ? argv["_"][1] : "~";
